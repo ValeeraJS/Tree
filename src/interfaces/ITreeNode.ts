18e39d4349d4708f1e568aa72991daa97bc73964
大聪明 <601670314@qq.com>
2024-02-02 00:30:05 -0800

    Update ITreeNode.ts

--- src/interfaces/ITreeNode.ts
+++ src/interfaces/ITreeNode.ts
@@ -1,11 +1,18 @@
-import { ITreeNode } from "./ITreeNode";
 import { IVisitor } from "./IVisitor";
 
-export interface IBinaryTreeNode extends ITreeNode {
-	children: Array<IBinaryTreeNode | null>;
-	left: IBinaryTreeNode | null;
-	parent: IBinaryTreeNode | null;
-	right: IBinaryTreeNode | null;
+export interface ITreeNodeData {
+	parent: ITreeNodeData | null;
+	children: Array<ITreeNodeData | null>;
+}
 
-	traverseInorder: (visitor: IVisitor<IBinaryTreeNode>, ...rest: any) => this;
+export interface ITreeNode extends ITreeNodeData {
+	addChild: (node: ITreeNode) => this;
+	depth: (node: ITreeNode) => number;
+	findLeaves: () => ITreeNode[];
+	findRoot: (node: ITreeNode) => ITreeNode;
+	hasAncestor: (node: ITreeNode) => boolean;
+	isLeaf: () => boolean;
+	removeChild: (node: ITreeNode) => this;
+	traversePostorder: (visitor: IVisitor<ITreeNode>, ...rest: any) => any;
+	traversePreorder: (visitor: IVisitor<ITreeNode>, ...rest: any) => any;
 }
