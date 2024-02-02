21dcaf17ff70dac2540aec4fd5c8d7dc001710f5
大聪明 <601670314@qq.com>
2024-02-02 00:30:19 -0800

    Update IBinaryTreeNode.ts

--- src/interfaces/IBinaryTreeNode.ts
+++ src/interfaces/IBinaryTreeNode.ts
@@ -1,13 +1,11 @@
 import { ITreeNode } from "./ITreeNode";
 import { IVisitor } from "./IVisitor";
 
-export interface IBinaryTreeNode<T> extends ITreeNode<T> {
-	children: Array<IBinaryTreeNode<T> | null>;
-	left: IBinaryTreeNode<T> | null;
-	parent: IBinaryTreeNode<T> | null;
-	right: IBinaryTreeNode<T> | null;
+export interface IBinaryTreeNode extends ITreeNode {
+	children: Array<IBinaryTreeNode | null>;
+	left: IBinaryTreeNode | null;
+	parent: IBinaryTreeNode | null;
+	right: IBinaryTreeNode | null;
 
-	traverseInOrder: (visitor: IVisitor<T>, rest: any) => this;
-	traversePostOrder: (visitor: IVisitor<T>, rest: any) => this;
-	traversePreOrder: (visitor: IVisitor<T>, rest: any) => this;
+	traverseInorder: (visitor: IVisitor<IBinaryTreeNode>, ...rest: any) => this;
 }
